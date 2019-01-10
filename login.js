9f46248fb07de87e8963affa7e341bfeb1ca015a
zhangdan183 <47217805@qq.com>
2019-01-10 17:35:50 +0800

    解决BUG
    解决服务器返回格式错误

--- login.js
+++ login.js
@@ -1,14 +1,20 @@
 var querystring = require('querystring');
+var util = require("util");
 function login(req, res){
 	var post = '';
 	req.on('data', function(chunk){
 		post += chunk;
 	});
+
+	var requsetmsg = {};
+
 	req.on('end', function(){    
         post = querystring.parse(post);
         console.log(post);
+        requsetmsg = {successmsg: 'ok'};
         res.writeHead(200, {"Content-Type": "text/plain; charset=utf-8"});
-		res.write("正常进入");
+        res.write(util.inspect(requsetmsg));
+		//res.write(requsetmsg);
 		res.end();
     });
 }
